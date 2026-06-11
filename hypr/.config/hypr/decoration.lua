3bbbcf4046b3752f7de1994423fbdc2909768f29
BitSwapped <228873900+BitSwapped@users.noreply.github.com>
2026-06-11 18:24:59 -0500

    hypr: add decorations conf file

---
+++ hypr/.config/hypr/decoration.lua
@@ -0,0 +1,24 @@
+--------------------------
+---- Decoration Setup ----
+--------------------------
+
+hl.config({
+	decoration = {
+		rounding = 6,
+		rounding_power = 3.7,
+		active_opacity = 1.0,
+		inactive_opacity = 0.9,
+		fullscreen_opacity = 1.0,
+
+		blur = {
+			enabled = true,
+			size = 12,
+			passes = 3,
+			noise = 0.2,
+			vibrancy = 0.8,
+		},
+		shadow = {
+			enabled = false,
+		},
+	},
+})
