3b04aff1ce3151d74167ce60ab1c9979946c92eb
BitSwapped <228873900+BitSwapped@users.noreply.github.com>
2026-06-11 15:35:39 -0500

    hypr: add monitor conf file

---
+++ hypr/.config/hypr/monitors.lua
@@ -0,0 +1,13 @@
+-----------------------
+---- Monitor Setup ----
+-----------------------
+
+hl.monitor({
+	output = "HDMI-A-1",
+
+	mode = "1920x1080@60Hz",
+
+	position = "0x0",
+
+	scale = 1.0,
+})
