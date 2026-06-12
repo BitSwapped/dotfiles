d02cbdd2355e3b6d9cb566a0d3d477d61b1e872a
BitSwapped <228873900+BitSwapped@users.noreply.github.com>
2026-06-11 21:12:57 -0500

    hypr: add layout conf file

---
+++ hypr/.config/hypr/layouts.lua
@@ -0,0 +1,22 @@
+------------------------
+---- Layouts Setup ----
+------------------------
+
+hl.config({
+	dwindle = {
+		preserve_split = true,
+		smart_split = false,
+		smart_resizing = false,
+	},
+
+	-- Master layout is handled here if needed
+	master = {
+		-- new_status = "master" -- Commented out due to compatibility reasons
+	},
+
+	binds = {
+		workspace_back_and_forth = false,
+		allow_workspace_cycles = true,
+		pass_mouse_when_bound = false,
+	},
+})
