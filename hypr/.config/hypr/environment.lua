0f2f1bdf6c8583b00b9a75e07a4abf42b05e85d1
BitSwapped <228873900+BitSwapped@users.noreply.github.com>
2026-06-13 18:58:44 -0500

    qt: add qt6ct conf file

--- hypr/.config/hypr/environment.lua
+++ hypr/.config/hypr/environment.lua
@@ -15,7 +15,7 @@ hl.env("CLUTTER_BACKEND", "wayland")
 
 hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
-hl.env("QT_QPA_PLATFORMTHEME", "qt5ct")
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
